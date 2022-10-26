53de5a362c42c231d34152894331cdc8e8d5b8f7
Mr. Kiedes <akiedes@vtsd.com>
2022-10-26 09:24:08 -0400

    Initial Upload

---
+++ Temp.java
@@ -0,0 +1,17 @@
+// Lab05vst.java
+// This is the student, starting version of the Lab05 assignment.
+
+
+import java.awt.*;
+import java.applet.*;
+
+
+public class Temp extends Applet
+{
+    public void paint(Graphics g)
+    {
+
+
+
+    }
+}
