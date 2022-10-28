cdbc17f112f7d5a8b87dc5c58eb593f6f0be5554
zach <zachary.schwerdtfeger@vtsd.com>
2022-10-28 11:46:24 -0400

    aahg

--- Lab05vst.java
+++ Lab05vst.java
@@ -6,16 +6,34 @@ import java.awt.*;
 import java.applet.*;
 
 
-public class Lab05vst extends Applet
-{
-    public void paint(Graphics g)
-    {
-        int width = 980;
-        int height = 630;
-        int x1 = 10;
-        int y1 = 640;
-        int x2 = 990;
-        int y2 = 640;
-        g.drawRect(10,10,width,height);
+public class Lab05vst extends Applet {
+    public void paint(Graphics g) {
+            int width = 980;
+            int height = 630;
+            int x1 = 10;
+            int y1 = 640;
+            int x2 = 990;
+            int y2 = 640;
+            g.drawRect(10, 10, width, height);
+
+
+            for (int k =0; k <= 100; k++) {
+
+                g.drawLine( x1, y1,x2 ,y2);
+                x1+=10;
+                y2-=6;
+            }
+
+            x1 = 10;
+            y1 = 640;
+            x2 = 990;
+            y2 = 640;
+            for(int b =0; b <=100; b++) {
+                g.drawLine(x1, y1, x2, y2);
+                y1 -= 6;
+                x2 -= 10;
+
+            }
+
     }
 }
--- Temp.java
+++ Temp.java
@@ -10,6 +10,15 @@ public class Temp extends Applet
 {
     public void paint(Graphics g)
     {
+        int x = 20;
+        int y = 20;
+        int width = 30;
+        int height = 30;
+        for(int k = 0; k < 50; k++){
+            g.drawOval(x, y, width,height);
+            x += 5;
+            y += 5;
+        }
 
 
 
